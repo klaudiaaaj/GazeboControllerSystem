e586952834b9a114ddedee2856973acfdbf3c10d
Klaudia <klaujan456@student.polsl.pl>
2023-07-10 23:11:29 +0200

    09.07

---
+++ Master/Publisher/RESTClient.cs/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTClient.cs.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
